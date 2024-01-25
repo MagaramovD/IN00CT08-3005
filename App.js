d175d16a4ba391cc1d7900874b116555833f523e
Din Magaramov <din.magaramov@tracker.fi>
2024-01-25 20:54:59 +0200

    did tehtävä 2

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Appbar, TextInput, Button } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Login = () => {
+    const [formData, setFormData] = useState({ username: '', password: '' });
+    const [errors, setErrors] = useState({ username: false, password: false });
+
+    const validateAndSubmit = () => {
+        const newErrors = {
+            username: !formData.username,
+            password: !formData.password,
+        };
+        setErrors(newErrors);
+
+        const isValid = !newErrors.username && !newErrors.password;
+        if (isValid) {
+            console.log("data form", formData);
+
+        }
+    };
+
+    return (
+        <SafeAreaProvider>
+            <View style={styles.container}>
+                <Appbar.Header>
+                    <Appbar.Content title="Login" />
+                </Appbar.Header>
+                <TextInput
+                    label="Username"
+                    value={formData.username}
+                    onChangeText={(text) => setFormData({ ...formData, username: text })}
+                    error={errors.username}
+                    keyboardType="default"
+                    style={styles.input}
+                />
+                <TextInput
+                    label="Password"
+                    value={formData.password}
+                    onChangeText={(text) => setFormData({ ...formData, password: text })}
+                    error={errors.password}
+                    secureTextEntry
+                    style={styles.input}
+                />
+                <Button mode="contained" onPress={validateAndSubmit} style={styles.button}>
+                    Login
+                </Button>
+            </View>
+        </SafeAreaProvider>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 8,
+    },
+    input: {
+        marginBottom: 8,
+    },
+    button: {
+        marginTop: 8,
+    },
 });
+
+export default Login;
